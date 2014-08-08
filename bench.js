7dc0d49e9be5124bb720b47fe4d2b94e34159177
Elijah Insua <tmpvar@gmail.com>
2014-08-08 01:03:33 -0700

    fix benchmark

--- bench.js
+++ bench.js
@@ -4,18 +4,23 @@ var throughputStream = fsm({
   init: fsm.want(1, function() {})
 });
 
-var l = 50000000;
-var buffers = Array(l).map(function() { return new Buffer(1024); });
+var l = 5000, i=0;
+var buffers = Array(l)
+
+for (i=0; i<l; i++) {
+  buffers[i] = new Buffer(1024);
+}
 
 var microtime = require('microtime');
 var start = microtime.nowDouble();
 
-for (var i=0; i<l; i++) {
+for (i=0; i<l; i++) {
   throughputStream.write(buffers[i]);
 }
 
+var time = microtime.nowDouble() - start;
 console.log(
-  'took %s with a throughput of %s bytes',
-  microtime.nowDouble() - start,
-  l*1024
+  'took %s with a throughput of %s bps',
+  time,
+   (l*1024) / time
 );
